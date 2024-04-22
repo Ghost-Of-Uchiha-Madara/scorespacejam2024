1fdff506c3db169397bd37ae4dcdeecfde9bc4fc
Madara Uchiha <sankar47m@gmail.com>
2024-04-22 21:28:20 +0530

    Main menu Added.

---
+++ Assets/Scripts/MainMenuButtonCondition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonCondition : MonoBehaviour
+{
+    public GameObject optionPannel;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionPannelEnable()
+    {
+        optionPannel.SetActive(true);
+    }
+
+    public void OptionPannelDisable()
+    {
+        optionPannel.SetActive(false);
+    }
+}
